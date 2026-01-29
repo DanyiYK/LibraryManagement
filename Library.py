4faa0c899ff2245eb3f0c6c4215f86aaadf7ab24
DanyiYK <danielranieri06@gmail.com>
2026-01-29 11:44:42 +0100

    Completed Library class

--- Library.py
+++ Library.py
@@ -1,29 +1,44 @@
-import User
-import Book
+from User import User
+from Book import Book
 
 class Library:
     def __init__(self, name):
         self.name = name
-        self.catalog = []
+        self.books = []
         self.users = []
 
     def __repr__(self):
-        return f"Library(name={self.name}, catalog={self.catalog}, users={self.users})"
+        return f"Library(name={self.name}, catalog={self.books}, users={self.users})"
 
-    def addBook(self, name, isbn, author, count=1):
-        pass
+    def addBook(self, book):
+        self.books.append(book)
 
-    def addUser(self, name):
-        pass
-
+    def addUser(self, User):
+        self.users.append(User)
+        
     def viewBooks(self):
-        pass
+        print("Registered books")
+        for book in self.books:
+            print(book)
 
     def viewUsers(self):
-        pass
+        print("Registered users:")
+        for user in self.users:
+            print(user)
 
     def searchBooks(self, name):
-        pass
+        name = name.lower()
+        found_books = []
 
-    def getUserByBadgeId(self, badgeId):
-        pass
+        for book in self.books:
+            adjusted_title = book.title.lower()
+
+            if adjusted_title.startswith(name):
+                found_books.append(book)
+        
+        return found_books
+
+    def getUserByBadgeNumber(self, badgeNumber):
+        for user in self.users:
+            if user.badgeId == badgeNumber:
+                return user
--- User.py
+++ User.py
@@ -1,27 +1,42 @@
+from Operation import Operation
+from Book import Book
+
+
+MAX_BORROWED_BOOKS_PER_USER = 3
+
 badge_number_counter = 0
 
 class User:
     def __init__(self, name, badgeNumber=None):
         self.name = name
+        self.borrowed = []
+
         badge_number_counter += 1
 
         if badgeNumber:
-            self.badge_number = badgeNumber
+            self.badgeNumber = badgeNumber
         else:
             # Badge number is equal to the first 3 letters of the user name followed by a number
-            self.badge_number = name[:3].lower() + badge_number_counter
+            self.badgeNumber = name[:3].lower() + badge_number_counter
 
     def __eq__(self, other):
         if not isinstance(other, User):
             return False
         
-        return self.badge_number==other.badge_number
+        return self.badgeNumber==other.badgeNumber
 
     def __repr__(self):
         return f"name={self.name}, badge_number={self.badge_number}"
     
     def borrowBook(self, book):
-        pass
+        if len(self.borrowed) >= MAX_BORROWED_BOOKS_PER_USER:
+            return
+        
+        if not book.isAvailable():
+            return
+        
+
+
 
     def returnBook(self, book):
         pass
