8d6d5225e853c2c93b346832cc6b01c7ad2ee539
DanyiYK <danielranieri06@gmail.com>
2026-01-29 11:06:38 +0100

    Added Book structure

---
+++ Book.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self, title, isbn, author, count=1):
+        self.title = title
+        self.isbn = isbn
+        self.author = author
+        self.count = count
+        self.inStock = count
+
+    def __eq__(self, other):
+        if not isinstance(other, Book):
+            return False
+        
+        return self.isbn
+    
+    def __repr__(self):
+        return f"Book(title={self.title}, isbn={self.isbn}, author={self.author}, count={self.count}, inStock={self.inStock})"
+    
+    def isAvailable(self):
+        pass
